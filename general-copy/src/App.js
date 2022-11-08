abe783175d14cd4b6c8a0fdcd8dcd3e4e7a78da3
Amal Medlege <amal@Amals-MacBook-Pro-2.local>
2022-11-08 22:46:32 +0200

    1st

--- general-copy/src/App.js
+++ general-copy/src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import Message from "./Components/Message";
 
 function App() {
-  return (
-    <div className="app">
-      <Message />
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
