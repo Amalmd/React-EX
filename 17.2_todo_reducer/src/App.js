628931a7cfc5914ca52ef3fde7babb1d2f41cbeb
Amal Medlege <amal@Amals-MacBook-Pro-2.local>
2022-11-16 02:13:24 +0200

    17.2 not finished

---
+++ 17.2_todo_reducer/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoHandler from "./Components/TodoHandler";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoHandler />
+    </div>
+  );
+}
+
+export default App;
