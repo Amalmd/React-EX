aec0646fd58ed1d66dd87a0fdd4282271e756791
Amal Medlege <amal@Amals-MacBook-Pro-2.local>
2022-11-23 00:34:45 +0200

    19.2 needs more work

---
+++ 19.2_mood_change/src/Components/DarkPage.js
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+
+function DarkPage() {
+  return <div></div>;
+}
+
+export default DarkPage;
---
+++ copy/src/Components/LightPage.js
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import AuthContext from "./context";
+
+function LightPage() {
+  return <div></div>;
+}
+
+export default LightPage;
